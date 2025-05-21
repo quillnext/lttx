6e8d7919643b4b54cd8e52667ef25763b017ba4d
Nikhil Verma <quillnext2024@gmail.com>
2025-05-22 00:11:26 +0530

    vercel issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ["picsum.photos","cloud.appwrite.io","firebasestorage.googleapis.com"],
 
