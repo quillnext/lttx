188b976abbf16d0c02392be96f41af324dad423c
Nikhil Verma <quillnext2024@gmail.com>
2025-05-22 00:09:35 +0530

    Updated Next.js export config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ["picsum.photos","cloud.appwrite.io","firebasestorage.googleapis.com"],
 
