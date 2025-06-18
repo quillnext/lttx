46975f1d57da7aec7c8dcc455b90dff9e7e9ec36
Nikhil Verma <quillnext2024@gmail.com>
2025-06-18 08:42:03 +0530

    gmail removed

--- src/app/api/send-reply-email/route.js
+++ src/app/api/send-reply-email/route.js
@@ -3,11 +3,13 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+  host: 'smtp.zoho.in',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
 });
 
 const emailTemplate = ({ userName, expertName, question, reply, year, type }) => `
